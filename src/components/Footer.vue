f9157d3426a0ded5e477654649988fd59758e0ab
guxuerui <im.guxuerui@gmail.com>
2024-12-03 14:15:25 +0800

    style(Footer.vue): adjust image positioning in the footer for better layout and visibility

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -11,5 +11,16 @@ const goBack = () => router.back()
     <div class="markdown-body">
       <a href="/">cd ~</a>
     </div>
+    <div class="markdown-body relative h-12">
+      <img class="visit-img" scale-30 src="https://count.getloli.com/@minecraft?name=minecraft&theme=minecraft&padding=7&offset=0&align=center&scale=1&pixelated=1&darkmode=auto" alt="">
+    </div>
   </nav>
 </template>
+
+<style>
+.visit-img {
+  position: absolute;
+  top: -130%;
+  left: -35%;
+}
+</style>
